2db498de92f01a9910de30af6d5faaac31d1dd76
Daniel <dev.daniil.bakushkin@gmail.com>
2024-11-06 20:20:16 +0300

    add ml

--- backend/whine_meter/ml.py
+++ backend/whine_meter/ml.py
@@ -1,6 +1,36 @@
-from random import random
+import time
+import requests
+
+# format
+# if error:
+#   returns a dict with d['error'] = "... currently loading"
+# else:
+#   returns a list
 
 
 def calculate_whine(message: str):
-    # TODO
-    return random()
+    API_URL = "https://api-inference.huggingface.co/models/seara/rubert-tiny2-russian-sentiment"
+    headers = {"Authorization": "Bearer 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",
+               'x-use-cache': 'true'}
+
+    def query(payload):
+        response = requests.post(API_URL, headers=headers, json=payload)
+        return response.json()
+
+    while True:
+        output: dict = query({
+            "inputs": message,
+        })
+        if isinstance(output, dict):
+            if 'error' in output.keys():
+                if 'currently loading' in output['error']:
+                    # print("time to sleep:", output['estimated_time'])
+                    time.sleep(output['estimated_time'])
+                else:
+                    raise BaseException("Unknown Error")
+            else:
+                raise BaseException("Unknown dict format")
+        elif isinstance(output, list):
+            return output
+        else:
+            raise BaseException("Unknown data structure")
