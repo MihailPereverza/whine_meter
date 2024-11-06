a6ec33c549061c315ccc3da1938f605f1a3292ab
Daniel <dev.daniil.bakushkin@gmail.com>
2024-11-06 21:48:14 +0300

    catch except in ml

--- backend/whine_meter/ml.py
+++ backend/whine_meter/ml.py
@@ -1,6 +1,6 @@
 import time
 import requests
-
+from random import random
 # format
 # if error:
 #   returns a dict with d['error'] = "... currently loading"
@@ -17,32 +17,36 @@ def calculate_whine(message: str):
         response = requests.post(API_URL, headers=headers, json=payload)
         return response.json()
 
-    counter = 0
-    while True:
-        output: dict = query({
-            "inputs": message,
-        })
-        if isinstance(output, dict):
-            if 'error' in output.keys():
-                if 'currently loading' in output['error']:
-                    # print("time to sleep:", output['estimated_time'])
-                    time.sleep(output['estimated_time'])
+    try:
+        counter = 0
+        while True:
+            output: dict = query({
+                "inputs": message,
+            })
+            if isinstance(output, dict):
+                if 'error' in output.keys():
+                    if 'currently loading' in output['error']:
+                        # print("time to sleep:", output['estimated_time'])
+                        time.sleep(output['estimated_time'])
+                    else:
+                        raise BaseException(
+                            "Unhandled Error:", output['error'])
                 else:
-                    raise BaseException("Unhandled Error:", output['error'])
+                    raise BaseException("Unknown dict format")
+            elif isinstance(output, list):
+                neg = 0
+                pos = 0
+                for el in output[0]:
+                    if el['label'] == 'negative':
+                        neg = el['score']
+                    if el['label'] == 'positive':
+                        pos = el['score']
+                return max(0, neg - pos)
             else:
-                raise BaseException("Unknown dict format")
-        elif isinstance(output, list):
-            neg = 0
-            pos = 0
-            for el in output[0]:
-                if el['label'] == 'negative':
-                    neg = el['score']
-                if el['label'] == 'positive':
-                    pos = el['score']
-            return max(0, neg - pos)
-        else:
-            raise BaseException("Unknown data structure:", type(output))
+                raise BaseException("Unknown data structure:", type(output))
 
-        counter += 1
-        if counter == 10:
-            raise BaseException("attempts timeout:", counter)
+            counter += 1
+            if counter == 10:
+                raise BaseException("attempts timeout:", counter)
+    except:
+        return random()
