737f3120645fbb88cd7aabffeafb66eefdd49c2a
Wizzerinus <development@toontowneventhorizon.com>
2024-11-06 18:07:56 +0300

    backend: aggregate get_users_data

--- backend/whine_meter/aggregation.py
+++ backend/whine_meter/aggregation.py
@@ -1,8 +1,21 @@
 from datetime import datetime, timedelta
 
+from sqlalchemy import select, func
+
+from whine_meter import model
+from whine_meter.core import db_session
+
 
 def get_users_data(chat_id: int):
-    return {"Студент": 0.8, "Менеджер программы": 0.1}  # todo: select from db
+    with db_session() as session:
+        query = (
+            select(model.User.username, func.avg(model.Message.whine_value))
+            .group_by(model.User.username)
+            .join(model.User)
+            .where(model.Message.chat_id == chat_id)
+        )
+
+        return dict(session.execute(query).fetchall())
 
 
 def get_dates_data(chat_id: int):
